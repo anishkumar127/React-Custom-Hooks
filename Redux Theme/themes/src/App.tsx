546d4be5943a1a83e88d235bee33da6d992cf8c0
Anish Kumar <anish.k@cubiclogics.com>
2023-06-30 00:20:49 +0530

    themes - light/dark/site

--- Redux Theme/themes/src/App.tsx
+++ Redux Theme/themes/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from "./store/hooks";
 import { setThemesMode } from "./store/slices/themes/themesSlice";
 
@@ -5,40 +6,23 @@ function App() {
   const mode = useAppSelector((state) => state.lightDarkMode.mode);
   const dispatch = useAppDispatch();
 
-  // const handleOnClickThemesMode = (selectedMode: string) => {
-  //   dispatch(setThemesMode(selectedMode));
-  // }
-
   const handleOnClickThemesMode = (mode: string) => () => {
     dispatch(setThemesMode(mode));
   };
 
+  useEffect(() => {
+    if (mode === "light") {
+      document.body.style.backgroundColor = "#f3f2f1";
+    } else if (mode === "dark") {
+      document.body.style.backgroundColor = "#212121";
+    } else {
+      document.body.style.backgroundColor = "white";
+    }
+  }, [mode]);
+
   return (
     <>
       <span>Theme mode is: {mode}</span>
-
-      {/* <div>
-        <label>
-          <input type="checkbox" checked={mode === "light"} onChange={() => handleOnClickThemesMode("light")} />
-          Light Mode
-        </label>
-      </div>
-
-      <div>
-        <label>
-          <input type="checkbox" checked={mode === "dark"} onChange={() => handleOnClickThemesMode("dark")} />
-          Dark Mode
-        </label>
-      </div>
-
-      <div>
-        <label>
-          <input type="checkbox" checked={mode === "site"} onChange={() => handleOnClickThemesMode("site")} />
-          Site Mode
-        </label>
-      </div>
-       */}
-
       <div>
         <label>
           <input
